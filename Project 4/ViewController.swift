6adc787dcd32eb13fa15c44deef4c075b0b29bb6
Mohamed Al Mohannadi <annexmoe1@mohameds-mini.hotspot>
2017-06-08 06:06:31 +0300

    Initial Commit

---
+++ Project 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 4
+//
+//  Created by Mohamed Al Mohannadi on 6/8/17.
+//  Copyright © 2017 Mohamed Al Mohannadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
